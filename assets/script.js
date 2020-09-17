3fbe806dbfaf0326e6d62604610bc152307dcc3d
Juliette Gondon <juliettegondon@gmail.com>
2020-09-16 22:16:19 -0400

    recent changes

--- assets/script.js
+++ assets/script.js
@@ -55,6 +55,7 @@ $( "#btn" ).on('click', function(event) {
         var lat = response.city.coord.lat;
         console.log(lat, lon)
 //ajax call to uvindex using variables from 5 day
+        // uv index call retrieved value and used to change class color
         var uvQuery = "http://api.openweathermap.org/data/2.5/uvi/forecast?&appid=c84a8aa0a886f082f2e927a6d245e491&lat=" +lat + "&lon=" + lon;
         $.ajax({
             url: uvQuery,
@@ -63,19 +64,40 @@ $( "#btn" ).on('click', function(event) {
             var uvIndex = secondResponse[0].value;
             console.log(uvIndex);
             $("#uvIndex").text("UV index: " + uvIndex);
+            if (uvIndex <= 2) {
+                $("#uvIndex").addClass("favorableUV");
+              } else if (uvIndex <= 5) {
+                $("#uvIndex").addClass("moderateUV");
+              } else if (uvIndex <= 11) {
+                $("#uvIndex").addClass("severeUV");}
         })
+
+        //5 day forecast ajax call
+        $.ajax({
+            url: fivedayURL + cityName + "&units=imperial" + apiKey,
+            method: "GET",
+        })
+        .then(function(fivedayResponse){
+            var fivedayTemp = fivedayResponse.list[0].main.temp;
+            console.log(fivedayTemp);
+            var fivedayHumid = fivedayResponse.list[0].main.humidity;
+            console.log(fivedayHumid);
+            $("#temp1").text("Temperature: " + fivedayTemp);
+            $("#humid1").text("Humidity: " + fivedayHumid);
+            $("#temp2").text("Temperature: " + fivedayTemp);
+            $("#humid2").text("Humidity: " + fivedayHumid);
+            $("#temp3").text("Temperature: " + fivedayTemp);
+            $("#humid3").text("Humidity: " + fivedayHumid);
+            $("#temp4").text("Temperature: " + fivedayTemp);
+            $("#humid4").text("Humidity: " + fivedayHumid);
+            $("#temp5").text("Temperature: " + fivedayTemp);
+            $("#humid5").text("Humidity: " + fivedayHumid);
+        })
+        
     })
-    //////ask during office hours why not working
- /*    if (uvIndex <= 2) {
-        $("#uvIndex").addClass(".green");
-      } else if (uVIndex <= 5) {
-        $("#uvIndex").addClass(".yellow");
-      } else if (uVIndex <= 8) {
-        $("#uvIndex").addClass(".red");} */
 
 });
 
 //create history for search in local storage
 //append city, date, and icon
-//get UV index
-//change color based on UV index
+
